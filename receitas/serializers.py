621fa9ea469b94a9d010810efe95c9447cbce6d6
Cristiane <christianeolive@hotmail.com>
2021-05-24 09:19:26 -0300

    FEACT adicionando models serializers

--- receitas/serializers.py
+++ receitas/serializers.py
@@ -1,3 +1,14 @@
+from django.db.models import fields
 from rest_framework import serializers
 
 from .models import Receitas
+
+class ReceitasSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Receitas
+        fields = {
+            'nome_receita',
+            'ingredientes',
+            'modo_preparo'
+
+        }
